ff3db9f8d452fc1bdb83d56e23a09f397b57c4a9
Rajat619 <rajatjainwal5@gmail.com>
2022-09-08 22:03:03 +0530

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,93 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }  
+      
+    public function customLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+   
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                        ->withSuccess('Signed in');
+        }
+  
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+
+    public function registration()
+    {
+        return view('auth.register');
+    }
+      
+    public function customRegistration(Request $request)
+    {  
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'bio' => 'required|min:2',
+            'address' => 'required|min:6',
+            'phone' => 'required|min:10|max:10'
+        ]);
+           
+        $data = $request->all();
+        $check = $this->create($data);
+         
+        return redirect("dashboard")->withSuccess('You have signed-in');
+    }
+
+    public function create(array $data)
+    {
+      return User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password']),
+        'bio' => $data['bio'],
+        'address' => $data['address'],
+        'phone' => $data['phone'],
+      ]);
+    }    
+    
+    public function dashboard()
+    {
+        if(Auth::check()){
+            $user = Auth::user();
+            return view('auth.profile', ['user' => $user]);
+        }
+  
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+    
+    public function signOut() {
+        Session::flush();
+        Auth::logout();
+  
+        return Redirect('login');
+    }
+
+    public function profile()
+    {
+        if (!Auth::check()) {
+            return view('auth.login');
+        }
+
+        $user = Auth::user();
+
+        return view('auth.profile', ['user' => $user]);
+    }
+}
